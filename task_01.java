7c3b90deda406658e671ce1c8223342c31fe73cc
MilyavichusEvgenii <milyavichus@yandex.ru>
2023-03-02 01:57:34 +0200

    fix error

--- task_01.java
+++ task_01.java
@@ -6,54 +6,35 @@ import java.util.Scanner;
 public class task_01 {
     
     public static void main(String[] args) {
-        int numb = GetNumb();
-        String[] arr = InitArr(numb);
-        String[] resArr = MathMethod(arr);
-        PrintArr(resArr);
+        double numb = getNumb();
+        double res = mathMethod(numb);
+        printArr(res);
 
 
     }
     //Ввод числа
-    public static int GetNumb() {
+    public static double getNumb() {
         Scanner get = new Scanner(System.in);
         System.out.println("Введите число: "); 
         try{
-            int a = get.nextInt();
+            double a = get.nextDouble();
             System.out.println();
             get.close();
             return a;
-        
         } catch(InputMismatchException ex) {
             get.close();
             System.out.println("Вы не ввели число");
         }
         return 0;
     }
-    //Инициализация массива
-    public static String[] InitArr(int a) { 
-        String[] temp = new String[a];
-        return temp;
+    public static double mathMethod(double numb) {
+        double result = (numb * 1 / 2) * (numb + 1);
+        return result; 
     }
-    //Заполнение массива
-    public static String[] MathMethod(String[] arr) {
-        String buffer = "";
-        for (int i = 0; i < arr.length; i++) {
-            if (i == 0) {
-                buffer = buffer + (i + 1);
-                arr[i] = buffer;
-            } else {
-                buffer = buffer + "+" + (i + 1);
-                arr[i] = buffer;
-            }
-                
-        }
-        return arr;
-    }
-    //Печать массива
-    public static void PrintArr(String[] arr) {
-        for (String i : arr) {
-            System.out.println(i);
-        }
+
+
+    public static void printArr(double num) {
+        System.out.printf("Триугольное число равняется %.0f", num);
         
     }
 }
